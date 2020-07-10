dd2addd45204cee14e585cdc1352734c0261c1b6
Tapan Vachhani <tapan.vachhani@zeuslearning.com>
2020-07-10 21:17:18 +0530

    dashboard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './modules/home/components/dashboard/dashboard.component';
+import { SearchResultsComponent } from './modules/home/components/search-results/search-results.component';
 
 
 const routes: Routes = [
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'search', component: SearchResultsComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
